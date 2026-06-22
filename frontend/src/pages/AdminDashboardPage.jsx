6ceba8800763ea61b5a0c11cb5057541ffcb5829
Olira1 <oliratesgera1@gmail.com>
2026-06-22 06:19:46 +0300

    save model performance metrics (Accuracy, F1-Score, Confusion Matrix)

--- frontend/src/pages/AdminDashboardPage.jsx
+++ frontend/src/pages/AdminDashboardPage.jsx
@@ -142,7 +142,7 @@ function AdminDashboardPage() {
         >
           {submitting
             ? "Saving..."
-            : ".gitignore to ignore datasets and virtual"}
+            : ".gitignore to ignore datasets and virtua"}
         </button>
       </form>
 
