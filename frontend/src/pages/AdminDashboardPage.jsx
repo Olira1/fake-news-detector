4b3e15753fa6e5f96112f12c2198f6527f3d9f79
Olira1 <oliratesgera1@gmail.com>
2026-06-22 06:14:41 +0300

    download and load Fake and True news .csv datasets

--- frontend/src/pages/AdminDashboardPage.jsx
+++ frontend/src/pages/AdminDashboardPage.jsx
@@ -142,7 +142,7 @@ function AdminDashboardPage() {
         >
           {submitting
             ? "Saving..."
-            : ".gitignore to ignore datasets and virtual environment"}
+            : ".gitignore to ignore datasets and virtual environmen"}
         </button>
       </form>
 
