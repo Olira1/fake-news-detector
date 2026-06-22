19f72f2be35d03c214cf587c569b9f2943063c12
Olira1 <oliratesgera1@gmail.com>
2026-06-22 06:16:37 +0300

    : document dataset sources and preprocessing pipeline

--- frontend/src/pages/AdminDashboardPage.jsx
+++ frontend/src/pages/AdminDashboardPage.jsx
@@ -142,7 +142,7 @@ function AdminDashboardPage() {
         >
           {submitting
             ? "Saving..."
-            : ".gitignore to ignore datasets and virtual environ"}
+            : ".gitignore to ignore datasets and virtual enviro"}
         </button>
       </form>
 
