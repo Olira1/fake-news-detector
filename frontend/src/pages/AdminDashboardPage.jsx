92d8bc934c40063bb603acd1c74be67b3f338c2e
Olira1 <oliratesgera1@gmail.com>
2026-06-22 06:15:04 +0300

    add column labels (1 for real, 0 for fake) and merge dataframes

--- frontend/src/pages/AdminDashboardPage.jsx
+++ frontend/src/pages/AdminDashboardPage.jsx
@@ -142,7 +142,7 @@ function AdminDashboardPage() {
         >
           {submitting
             ? "Saving..."
-            : ".gitignore to ignore datasets and virtual environmen"}
+            : ".gitignore to ignore datasets and virtual environme"}
         </button>
       </form>
 
